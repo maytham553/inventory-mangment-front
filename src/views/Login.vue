e2f7ab2d2bc1f54248c68b9a05c8bf3463afd450
Maytham mahdi <metham553@gmail.com>
2023-10-11 14:37:03 +0300

    IMS-30 - Front - Login form(style) -- tiny changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,24 @@
     <div class="w-screen h-screen bg-gray-100 grid grid-cols-3">
 
     <div class="h-screen col-span-1 p-5 bg-blue-500 flex flex-col justify-center items-center">
-                    <h1 class="text-gray-100 text-3xl">اهلاً بك مجدداً</h1><br>
-                    <p class="text-gray-100">أدخل معلوماتك للدخول الى حسابك</p>
+                    <h1 class="text-gray-100 font-black	 text-3xl">اهلاً بك مجدداً</h1><br>
+                    <p class="text-gray-100 font-bold ">أدخل معلوماتك للدخول الى حسابك</p>
                 </div>
         <div class="h-screen col-span-2  flex flex-col justify-center items-center pt-16">
 
-                <h1 class="text-blue-500 text-4xl">تسجيل الدخول</h1>
+                <h1 class="text-blue-500 text-4xl font-black">تسجيل الدخول</h1>
 
                 <form @submit.prevent="login" class="w-full h-screen flex flex-col justify-center items-center">
                     <input type="hidden" name="remember" value="true">
 
                     <input id="email-address" v-model="email" name="email" type="email" autocomplete="email" required
-                        class="pr-4 appearance-none border border-gray-400 focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
-                            bg-gray-200 w-2/5 h-10 pl-5 rounded text-gray-500 text-sm placeholder:text-gray-500"
+                        class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
+                            bg-gray-200 w-2/5 h-10 pl-5 rounded-xl text-gray-500 text-sm placeholder:text-gray-500"
                         :class="{ 'border-red-500': auth.status.error }" placeholder="البريد الإلكتروني"><br>
 
                     <input v-model="password" name="password" type="password" autocomplete="current-password" required
-                        class="pr-4 appearance-none border border-gray-400 focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
-                        bg-gray-200 w-2/5 h-10 pl-5 rounded text-gray-500 text-sm placeholder:text-gray-500"
+                        class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
+                        bg-gray-200 w-2/5 h-10 pl-5 rounded-xl text-gray-500 text-sm placeholder:text-gray-500"
                         :class="{ 'border-red-500': auth.status.error }" placeholder="كلمة المرور">
                     <br>
 
