8f4ba83907acdaf9c930ad4ac6c4e22996c71340
Maytham mahdi <metham553@gmail.com>
2023-10-12 09:10:39 +0300

    IMS-30 - Front - Login form(style) -- Error message apper

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,37 +2,38 @@
 <template>
     <div class="w-screen h-screen bg-gray-100 grid grid-cols-3">
 
-    <div class="h-screen col-span-1 p-5 bg-blue-500 flex flex-col justify-center items-center">
-                    <h1 class="text-gray-100 font-black	 text-3xl">اهلاً بك مجدداً</h1><br>
-                    <p class="text-gray-100 font-bold ">أدخل معلوماتك للدخول الى حسابك</p>
-                </div>
+        <div class="h-screen col-span-1 p-5 bg-blue-500 flex flex-col justify-center items-center">
+            <h1 class="text-gray-100 font-black	 text-3xl">اهلاً بك مجدداً</h1><br>
+            <p class="text-gray-100 font-bold ">أدخل معلوماتك للدخول الى حسابك</p>
+        </div>
         <div class="h-screen col-span-2  flex flex-col justify-center items-center pt-16">
 
-                <h1 class="text-blue-500 text-4xl font-black">تسجيل الدخول</h1>
+            <h1 class="text-blue-500 text-4xl font-black">تسجيل الدخول</h1>
 
-                <form @submit.prevent="login" class="w-full h-screen flex flex-col justify-center items-center">
-                    <input type="hidden" name="remember" value="true">
+            <form @submit.prevent="login" class="w-full h-screen flex flex-col justify-center items-center">
+                <input type="hidden" name="remember" value="true">
 
-                    <input id="email-address" v-model="email" name="email" type="email" autocomplete="email" required
-                        class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
+                <input id="email-address" v-model="email" name="email" type="email" autocomplete="email" required class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
                             bg-gray-200 w-2/5 h-10 pl-5 rounded-xl text-gray-500 text-sm placeholder:text-gray-500"
-                        :class="{ 'border-red-500': auth.status.error }" placeholder="البريد الإلكتروني"><br>
+                    :class="{ 'border-red-500': auth.status.error }" placeholder="البريد الإلكتروني"><br>
 
-                    <input v-model="password" name="password" type="password" autocomplete="current-password" required
-                        class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
+                <input v-model="password" name="password" type="password" autocomplete="current-password" required class="pr-4 appearance-none border  focus:ring-blue-500 focus:border-blue-500 focus:outline-none 
                         bg-gray-200 w-2/5 h-10 pl-5 rounded-xl text-gray-500 text-sm placeholder:text-gray-500"
-                        :class="{ 'border-red-500': auth.status.error }" placeholder="كلمة المرور">
-                    <br>
+                    :class="{ 'border-red-500': auth.status.error }" placeholder="كلمة المرور">
+                <br>
+                <div v-if="auth.status.error" class="text-red-500 text-center h-10 ">
+                    <span>{{ auth.status.message }}</span>
+                </div>
 
-                    <button type="submit" :disabled=auth.status.loading
-                        class="group relative w-1/3 flex justify-center gap-3 py-2 px-4 border border-transparent text-md font-medium rounded-2xl text-gray-100 bg-blue-500 hover:bg-blue-700 focus:outline-none ">
-                        تسجيل الدخول
-                       <Loading v-if="auth.status.loading"  />
-                    </button>
-                </form>
-            </div>
+                <button type="submit" :disabled=auth.status.loading
+                    class="group relative w-1/3 flex justify-center gap-3 py-2 px-4 border border-transparent text-md font-medium rounded-2xl text-gray-100 bg-blue-500 hover:bg-blue-700 focus:outline-none ">
+                    تسجيل الدخول
+                    <Loading v-if="auth.status.loading" />
+                </button>
+            </form>
+        </div>
 
-            
+
     </div>
 </template>
 
