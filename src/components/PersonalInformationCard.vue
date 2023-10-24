616f5e3497ae4f61c04bbd0d2877ba86a465c3f2
Haider Mohammed <haimohx@gmail.com>
2023-10-25 00:13:07 +0300

    IMS-35 some styling on the main div, calling the new component 'UserIcon' to hold the user icon svg and hiding the the note and the address section and also hiding alla the headers

--- src/components/PersonalInformationCard.vue
+++ src/components/PersonalInformationCard.vue
@@ -1,43 +1,47 @@
 <template>
     
-    <div class="p-4 max-w-sm max-h-sm bg-gray-100 drop-shadow-2xl rounded-lg flex flex-col w-content">
-       <div class="mb-2">
-        <img class="w-10 " src="src/icons/userIcon.png" alt="user's pic">
-       </div>
-        <div> 
+    <div class="p-4 max-w-sm max-h-sm bg-gray-100 drop-shadow-xl rounded-lg flex flex-col  w-content">
+       <div class="flex justify-center p-10 mb-5 bg-gradient-to-b from-sky-300 to-indigo-300 rounded">
+         <UserIcon size="150" strokeColor="rgba(0,0,0,0.1)" fillColor="#172350"/>
+           </div>
+        <div class="p-1 flex flex-col space-y-3"> 
+             <div>
+                <span class="font-bold"></span>
+                <span>{{ id }}</span>
+            </div>
         <div>
-            <span class="font-bold">الأسم : </span>
+            <span class="font-bold"></span>
             <span>{{ name }}</span>
         </div>
         <div v-if="email">
-            <span class="font-bold">البريد الإلكتروني : </span>
+            <span class="font-bold"></span>
             <span>{{ email }}</span>
         </div>
         <div>
-            <span class="font-bold">رقم الهاتف : </span>
+            <span class="font-bold"></span>
             <span>{{ phone }}</span>
         </div>
-        <div>
+        <!-- <div>
             <span class="font-bold">العنوان : </span>
             <span>{{ address }}</span>
-        </div>
+        </div> -->
         <div>
-            <span class="font-bold">المحافظة : </span>
+            <span class="font-bold"></span>
             <span>{{ governorate }}</span>
         </div>
-        <div v-if="note">
+        <!-- <div v-if="note">
           <span class="font-bold">ملاحظات : </span>
           <span>{{ showMore ? truncatedNote : note }}</span>
           <a v-if="showMore" @click="expandNote" class="cursor-pointer text-green-500 text-s">أكمل القراءة</a>
-        </div>
-        <div class="flex flex-row space-x-4 space-x-reverse mt-3 items-center">
-            <button @click="updateHandler" class="text-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        </div> -->
+        <div class="flex flex-row items-center justify-center w-full   ">
+            <button @click="updateHandler" class="text-xs bg-[#2b4162] hover:bg-[#1a2c47] text-white font-bold py-2 px-4 rounded-r-md">
                 تعديل
             </button>
-            <button @click="deleteHandler" class="text-xs bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            <button @click="deleteHandler" class="text-xs bg-[#d11a2a] hover:bg-[#a60c1c] text-white font-bold py-2 px-4 ">
                 حذف
             </button>
-            <button @click="showHandler" class=" text-xs bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <button @click="showHandler" class="text-xs bg-[#53917e] hover:bg-[#40765c] text-white font-bold py-2 px-4 rounded-l-md">
                 عرض
             </button>
         </div>
@@ -47,8 +51,13 @@
 
 <script setup lang="ts">
 import { computed, defineProps, ref } from 'vue'
+import UserIcon from '@/components/icons/UserIcon.vue';
 
 const props = defineProps({
+    id:{
+        type:Number,
+        required:true
+    },
     name: {
         type: String,
         required: true
