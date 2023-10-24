c578eeaa083ada7d309b57b5bef0f5829aa32f2e
Haider Mohammed <haimohx@gmail.com>
2023-10-25 00:05:35 +0300

    IMS-35 just changing the bg color to gray-100

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-wrap p-5 justify-center w-full gap-3 bg-gray-300 2xl:flex-row sm:flex-col ">
+    <div class="flex flex-wrap p-5 justify-center w-full gap-3 bg-gray-100 2xl:flex-row sm:flex-col ">
         <slot />
     </div>
 </template>
