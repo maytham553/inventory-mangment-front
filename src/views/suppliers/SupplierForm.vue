513af1515cc210ab3482634e151794ae8e0e3110
kainy01 <kainykawai@gmail.com>
2023-11-19 00:50:46 +0300

    styled form

--- src/views/suppliers/SupplierForm.vue
+++ src/views/suppliers/SupplierForm.vue
@@ -1,49 +1,54 @@
 <template>
-  <form @submit.prevent="handleSubmit" class="w-full h-full flex justify-center flex-col items-center">
-    <div class="bg-gray-100 p-5 sm:w-[400px] rounded-2xl">
-      <div class="mb-4">
-        <label for="name" class="block text-gray-700 m-1">الاسم</label>
-        <input type="text" id="name" v-model="formData.name" required class="form-input w-full p-1 rounded-lg" />
-      </div>
-      <div class="mb-4">
-        <label for="email" class="block text-gray-700 m-1">البريد الالكتروني</label>
-        <input type="email" id="email" v-model="formData.email" class="form-input w-full p-1 rounded-lg" />
-      </div>
-      <div class="mb-4">
-        <label for="phone" class="block text-gray-700 m-1 "> رقم الهاتف </label>
-        <input type="text" id="phone" v-model="formData.phone" required class="form-input w-full p-1 rounded-lg" />
-      </div>
-      <div class="mb-4">
-        <label for="address" class="block text-gray-700 m-1">العنوان</label>
-        <input type="text" id="address" v-model="formData.address" required class="form-input w-full p-1 rounded-lg" />
-      </div>
-      <div class="mb-4">
-        <label for="governorate_id" class="block text-gray-700 m-1">المحافظة</label>
-        <select id="governorate_id" v-model="formData.governorate_id" required class="form-select w-full p-1 rounded-lg">
-          <option value="" disabled>اختر المحافظة</option>
-          <option v-for="governorate in governorates" :key="governorate.id" :value="governorate.id">
-            {{ governorate.name }}
-          </option>
-        </select>
-      </div>
-      <div class="mb-4">
-        <label for="note" class="block text-gray-700 m-1">ملاحظة</label>
-        <textarea id="note" v-model="formData.note" class="form-textarea w-full p-1 rounded-lg"></textarea>
-      </div>
-      <div class="mb-4">
-        <span v-if="status.error" class="text-red-500 text-center h-5 ">
-          <span>{{ status.message }}</span>
-        </span>
-      </div>
-      <div class="mb-4">
-        <button type="submit"
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full flex justify-center gap-3"
-          :class="{ 'bg-blue-300': status.loading }" :disabled="status.loading">
-          {{ submitButtonText }}
-          <Loading v-if="status.loading" class="-mr-1 ml-3" />
-        </button>
-      </div>
-    </div>
+  <h1 class="text-blue-500 text-4xl  font-bold  text-center"> إضافة مورد جديد</h1>
+  <p class="my-5  text-gray-500 text-center">
+    اضف مورد جديد ادناه
+  </p>
+  <form @submit.prevent="handleSubmit"
+    class="w-full bg-gray-100 p-5 pt-10 sm:w-[400px] rounded-2xl mx-auto h-full flex gap-5 justify-center flex-col items-center">
+
+
+    <input type="text" id="name" v-model="formData.name" required placeholder="الاسم"
+      class="form-input w-full p-2 rounded-lg" />
+
+
+    <input type="email" id="email" v-model="formData.email" placeholder="البريد الالكتروني"
+      class="form-input w-full p-2 rounded-lg" />
+
+
+    <input type="text" id="phone" v-model="formData.phone" required placeholder="رقم الهاتف"
+      class="form-input w-full p-2 rounded-lg" />
+
+
+    <input type="text" id="address" v-model="formData.address" required placeholder="العنوان"
+      class="form-input w-full p-2 rounded-lg" />
+
+
+    <select id="governorate_id" v-model="formData.governorate_id" required class=" w-full p-2 cursor-pointer rounded-lg">
+      <option value="" disabled selected class=" w-full p-2 rounded-lg">اختر المحافظة</option>
+      <option v-for="governorate in governorates" :key="governorate.id" :value="governorate.id">
+        {{ governorate.name }}
+      </option>
+    </select>
+
+
+
+    <textarea id="note" v-model="formData.note" placeholder="ملاحظة"
+      class="form-textarea w-full p-2 rounded-lg"></textarea>
+
+
+    <span v-if="status.error" class="text-red-500 text-center h-5 ">
+      <span>{{ status.message }}</span>
+    </span>
+
+
+    <button type="submit"
+      class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg  flex justify-center gap-3"
+      :class="{ 'bg-blue-300': status.loading }" :disabled="status.loading">
+      {{ submitButtonText }}
+      <Loading v-if="status.loading" class="-mr-1 ml-3" />
+    </button>
+
+
   </form>
 </template>
 
